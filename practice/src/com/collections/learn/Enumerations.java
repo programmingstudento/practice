f8530cb2a10d501988427323c7d21c44cdbf4682
programmingstudento <utsabkarkee1@gmail.com>
2022-11-28 19:11:00 +0545

    StringTokenizer.

--- practice/src/com/collections/learn/Enumerations.java
+++ practice/src/com/collections/learn/Enumerations.java
@@ -2,6 +2,7 @@ package com.collections.learn;
 
 import java.util.Enumeration;
 import java.util.List;
+import java.util.StringTokenizer;
 import java.util.Vector;
 
 public class Enumerations {
@@ -10,6 +11,11 @@ public class Enumerations {
 		for (Enumeration<String> enumeration = new Vector<>(List.of("Orange", "Banana")).elements(); enumeration
 				.hasMoreElements();) {
 			System.out.println(enumeration.nextElement());
+		}
+
+		StringTokenizer stringTokenizer = new StringTokenizer("Orange Lemon Guava Watermelon");
+		for (; stringTokenizer.hasMoreTokens();) {
+			System.out.println(stringTokenizer.nextToken());
 		}
 	}
 }
