0ec314651d375d850ee78a933d09a31041658968
programmingstudento <utsabkarkee1@gmail.com>
2022-12-07 13:25:51 +0545

    AtomicInteger

---
+++ practice/src/com/thread/learn/ThreadNine.java
@@ -0,0 +1,43 @@
+package com.thread.learn;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ThreadNine {
+
+	public static void main(String[] args) {
+		Counter counter = new Counter();
+		System.out.println(counter.getAtomicInteger().intValue());
+		Runnable[] runnables = { () -> counter.increment(), () -> counter.increment(), () -> counter.increment(),
+				() -> counter.increment() };
+		ExecutorService executor = Executors.newFixedThreadPool(2);
+		for (Runnable runnable : runnables) {
+			executor.submit(runnable);
+		}
+		try {
+			executor.awaitTermination(3, TimeUnit.SECONDS);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println(counter.getAtomicInteger().intValue());
+		executor.shutdown();
+	}
+}
+
+class Counter {
+	private AtomicInteger atomicInteger;
+
+	public Counter() {
+		atomicInteger = new AtomicInteger(1);
+	}
+
+	public AtomicInteger getAtomicInteger() {
+		return atomicInteger;
+	}
+
+	public void increment() {
+		atomicInteger.incrementAndGet();
+	}
+}
