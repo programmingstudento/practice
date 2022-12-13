51ae4f5992857b88380725cedcc45324be890335
programmingstudento <utsabkarkee1@gmail.com>
2022-12-13 13:36:22 +0545

    Static/Early Binding.

---
+++ practice/src/com/home/football/Addition.java
@@ -0,0 +1,15 @@
+package com.home.football;
+
+final public class Addition {
+	public static int add(final int one, final int two) {
+		return one + two;
+	}
+
+	public static double add(final double one, final double two) {
+		return one + two;
+	}
+
+	public static String add(final String one, final String two) {
+		return one.concat(two);
+	}
+}
---
+++ practice/src/com/home/football/AdditionTest.java
@@ -0,0 +1,12 @@
+package com.home.football;
+
+public class AdditionTest {
+
+	public static void main(String[] args) {
+		System.out.println("Method Overloading example");
+		System.out.println(Addition.add(5.5, 4.5));
+		System.out.println(Addition.add(4, 6));
+		System.out.println(Addition.add("TE", "N"));
+	}
+
+}
